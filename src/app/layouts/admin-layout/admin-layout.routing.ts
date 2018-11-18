174dc2fe890d44a781b2bc7b60a376a0e4ab6d94
Jared Achi <jared.achi@fundacion-jala.org>
2018-11-17 20:07:05 -0400

    added basic courses list

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,21 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
 import { RegisterUserComponent } from '../../register-user/register-user.component';
+import { CourseListComponent } from '../../courses/course-list/course-list.component';
+import { CourseDescriptionComponent } from '../../courses/course-description/course-description.component';
+
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'user-profile', component: UserProfileComponent },
-    { path: 'table-list', component: TableListComponent },
-    { path: 'typography', component: TypographyComponent },
-    { path: 'icons', component: IconsComponent },
-    { path: 'maps', component: MapsComponent },
-    { path: 'notifications', component: NotificationsComponent },
     { path: 'register-user', component: RegisterUserComponent },
+    { path: 'course-list', component: CourseListComponent },
+    { path: 'course-description/:id', component: CourseDescriptionComponent },
 ];
