93ce68a973a8fedab16fa753d1f6f75ab966f944
Mauricio Gustavo Rojas Solis <gus.solis.15@gmail.com>
2018-12-02 19:00:28 +0000

    Deliver task

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -15,6 +15,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
 import { RegisterUserComponent } from '../../register-user/register-user.component'
 import { TaskComponent } from '../../task/task.component';
+import { DeliverTaskComponent } from '../../task/deliver-task/deliver-task.component';
 import { CourseListComponent } from '../../courses/course-list/course-list.component';
 import { CourseDescriptionComponent } from '../../courses/course-description/course-description.component';
 import { CourseRegisterComponent } from '../../courses/course-register/course-register.component';
@@ -40,6 +41,7 @@ import { TaskService } from '../../services/task.service';
     MapsComponent,
     RegisterUserComponent,
     TaskComponent,
+    DeliverTaskComponent,
     NotificationsComponent,
     CourseListComponent,
     CourseDescriptionComponent,
